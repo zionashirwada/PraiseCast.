7a026e56bb31caca52fbb4dc7aa01d3b3b990d2b
Ashirwada Manamudali <ashirwadamanamudali@gmail.com>
2026-04-03 20:21:28 +0530

    dev

--- src/electron/index.ts
+++ src/electron/index.ts
@@ -52,7 +52,7 @@ config.set("loaded", true)
 if (!config.get("loaded")) console.error("Could not get stored data!")
 
 // info
-console.info("Starting FreeShow...")
+console.info("Starting PraiseCast...")
 if (!isProd) console.info("Building app! (This may take 20-90 seconds)")
 
 // set application menu
